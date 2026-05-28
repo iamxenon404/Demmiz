e29e89802c0d851f07b9da75347c1c80bfbf3a21
xenon <demiladeafuape74@gmail.com>
2026-05-28 15:55:11 +0100

    imported collection grid in home page

--- frontend/app/Home/Page.tsx
+++ frontend/app/Home/Page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './components/Hero'
 import TrustBar from './components/TrustBar'
+import CollectionsGrid from './components/CollectionsGrid'
 
 const Homepage = () => {
   return (
     <>
     <Hero />
     <TrustBar />
+    <CollectionsGrid />
     </>
   )
 }
