69ccd454a77bae6e86e44eaa2138ffb51ef36bd6
xenon <demiladeafuape74@gmail.com>
2026-05-28 15:51:32 +0100

    imported trustbar in home page

--- frontend/app/Home/Page.tsx
+++ frontend/app/Home/Page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import TrustBar from './components/TrustBar'
 
 const Homepage = () => {
   return (
     <>
     <Hero />
+    <TrustBar />
     </>
   )
 }
