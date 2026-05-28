ff627d0d2215ee2673eec3dbeb254bda21eeb8c3
xenon <demiladeafuape74@gmail.com>
2026-05-28 15:23:43 +0100

    imported header in Layouts.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./component/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <>
-    
+    <Header />
     </>
   );
 }
