6c52b407814e6f211b5643aa6927f5a7626fb8c4
xenon <demiladeafuape74@gmail.com>
2026-05-28 16:00:55 +0100

    imported home components in homepage

--- frontend/app/Home/Page.tsx
+++ frontend/app/Home/Page.tsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import Hero from './components/Hero'
-import TrustBar from './components/TrustBar'
-import CollectionsGrid from './components/CollectionsGrid'
+import React from 'react';
+import Hero from './components/Hero';
+import TrustBar from './components/TrustBar';
+import CollectionsGrid from './components/CollectionsGrid';
+import TrendingProducts from './components/TrendingProducts';
+import DecantExperience from './components/DecantExperience';
+import BrandSpotlight from './components/BrandSpotlight';
+import Reviews from './components/Reviews';
 
 const Homepage = () => {
   return (
     <>
-    <Hero />
-    <TrustBar />
-    <CollectionsGrid />
+      {/* 1. Hero / Main Visual Section */}
+      <Hero />
+      
+      {/* 2. Value Propositions (Authenticity, Decants, Shipping) */}
+      <TrustBar />
+      
+      {/* 3. Visual Collections Grid (Niche, Samples, Rare) */}
+      <CollectionsGrid />
+      
+      {/* 4. Product Display Carousel/Grid (Trending Inventory) */}
+      <TrendingProducts />
+      
+      {/* 5. Educational / Explanatory Section (The Sample Process) */}
+      <DecantExperience />
+      
+      {/* 6. High-Impact Editorial Banner (Brand Focus) */}
+      <BrandSpotlight />
+      
+      {/* 7. Social Proof & Trust Badges (User Reviews) */}
+      <Reviews />
     </>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
