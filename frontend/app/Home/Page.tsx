932439ae241649c3fa483e1ddce56244a9e694ac
xenon <demiladeafuape74@gmail.com>
2026-05-28 15:41:45 +0100

    change home page func name to Home to match the import in root page

--- frontend/app/Home/Page.tsx
+++ frontend/app/Home/Page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from './components/Hero'
 
-const Page = () => {
+const Home = () => {
   return (
     <>
     <Hero />
@@ -9,4 +9,4 @@ const Page = () => {
   )
 }
 
-export default Page
+export default Home
